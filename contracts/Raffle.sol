4fe2d260f6ef8b24b895196b633943331a631ea2
Luciano <luciano@Lucianos-MacBook-Pro.local>
2022-08-08 15:52:53 -0300

    added contract and basic functions

---
+++ contracts/Raffle.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+error Raffle__NotEnoughETHEntered();
+
+// Raffle
+contract Raffle {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    // Enter the lottery (pay a certain ammount)
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+
+        // Events
+    }
+
+    // Pick a random winner (verifiably random)
+    // function pickRandomWinner() {}
+
+    // Winner to be selected every x minutes -> completly automated
+    // Chainlink Oracle -> Randomness, Automatic execution (Chainlink Keeper)
+
+    function getEntraceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
